fe9a858ddc23f1358d5c1137f524e3ae3d889af9
eugenekhoo <ktw1995@gmail.com>
2023-09-28 21:00:54 +0800

    updated playercontroller addplayer remove cap

--- server/controllers/playerController.js
+++ server/controllers/playerController.js
@@ -12,24 +12,20 @@ async function addPlayer(req, res) {
     [gid]
   );
 
-  if (checkPlayers.rows[0][teamCol].length < 4) {
-    if (
-      checkPlayers.rows[0][team1_players].includes(player) ||
-      checkPlayers.rows[0][team2_players].includes(player)
-    ) {
-      return res.status(400).json({ message: "Player already exists!" });
-    }
-    try {
-      const SQL_QUERY = `
+  if (
+    checkPlayers.rows[0]["team1_players"].includes(player) ||
+    checkPlayers.rows[0]["team2_players"].includes(player)
+  ) {
+    return res.status(400).json({ message: "Player already exists!" });
+  }
+  try {
+    const SQL_QUERY = `
           UPDATE games SET ${teamCol} = ARRAY_APPEND(${teamCol}, $1) WHERE game_id=$2
       `;
-      const response = await query(SQL_QUERY, [player, gid]);
-      return res.status(201).json({ message: "Player created!" });
-    } catch (err) {
-      console.error(err);
-    }
-  } else {
-    return res.status(400).json({ error: "Max players!" });
+    const response = await query(SQL_QUERY, [player, gid]);
+    return res.status(201).json({ message: "Player created!" });
+  } catch (err) {
+    console.error(err);
   }
 }
 
